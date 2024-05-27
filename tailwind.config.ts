a84328a4570a1eb5bcb2faf33b0434ef91979e20
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-05-27 13:31:43 -0400

    add: /docs, /components, /lib, LICENSE refactor tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        black: {
+          DEFAULT: "#000",
+          100: "#000313",
+          200: "#000627",
+          300: "#000940",
+          400: "#000c53",
+          500: "#000f66",
+          600: "#001279",
+          700: "#00168c",
+          800: "#00199f",
+          900: "#001cb2",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
