1928e3668f6d4572cd2491cd8f73703f3d9791e8
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-05-28 14:39:19 -0400

    modify: tailwind config, page.tsx add: SpotLight.tsx, cn.ts, require.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import HeroComponent from "@/components/HeroComponent";
-import Image from "next/image";
+import { Spotlight } from "@/components/ui/SpotLight";
 
 export default function Home() {
   return (
     <main className="relative flex justify-center items-center fle-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <HeroComponent />
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
+        <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="white" />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="white" />
       </div>
     </main>
   );
