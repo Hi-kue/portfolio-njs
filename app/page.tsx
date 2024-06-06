a0c32f52d317e0372dbf52103262acaec0f3c122
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-06-06 13:06:08 -0400

    refactor: {layout, page}.tsx, require.json, add: provider.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
 import { Spotlight } from "@/components/ui/SpotLight";
 
 export default function Home() {
-  return (
-    <main className="relative flex justify-center items-center fle-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
-        <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="white" />
-        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="white" />
-      </div>
-    </main>
-  );
+	return (
+		<div className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+			<div className="max-w-7xl w-full">
+				<Spotlight
+					className="-top-40 -left-10 md:-left-20 md:-top-10 h-[80vh]"
+					fill="white"
+				/>
+				<Spotlight
+					className="top-10 left-full h-[80vh] w-[60vw]"
+					fill="white"
+				/>
+				<Spotlight className="top-28 left-80 h-[60vh] w-[50vw]" fill="orange" />
+			</div>
+
+			<div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-dot-white/[0.3] bg-dot-black/[0.3] relative flex items-center justify-center">
+				<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+			</div>
+
+			<div className="flex justify-center">
+				<div className="max-w-[89vw]"></div>
+			</div>
+		</div>
+	);
 }
